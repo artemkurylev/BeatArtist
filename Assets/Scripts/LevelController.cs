b9528d6af26b28676da58193f07303316cadf702
Artem Kurylev <you@example.com>
2019-09-28 20:45:06 +0300

    Created level with random target generation and clicks

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour
+{
+    public GameObject target;
+    public Text Score;
+    public static LevelController lc;
+    public float score;
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = Time.time;
+        if (lc == null)
+            lc = this.gameObject.GetComponent<LevelController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float cur_time = Time.time;
+        if(cur_time - time > 3)
+        {
+            time = cur_time;
+            Vector3 pos = this.transform.position; 
+            Instantiate(target, GeneratePosition(), new Quaternion(0,0,0,0));
+        }
+    }
+    private Vector3 GeneratePosition()
+    {
+        float x, y, z;
+        z = -8;
+        x = Random.Range(-5, 5);
+        y = Random.Range(-3.5f, 3);
+        return new Vector3(x, y, z);
+    }
+    public void updateScore(float score)
+    {
+        this.score += score;
+        if (Score != null)
+        {
+            Score.text = this.score.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public const float LifeTime = 2.0f;
+    private float time;
+    public int maxScore = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = Time.time;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Click on object");
+            UpdateScore();
+            Destroy(this.gameObject);
+        }
+        if (Time.time - time > LifeTime)
+        {
+            Debug.Log("Time ended");
+            Destroy(this.gameObject);
+        }
+    }
+    void UpdateScore()
+    {
+        float clickTime = Time.time - time;
+        float score = (1 - clickTime / LifeTime) * maxScore;
+        LevelController.lc.updateScore(score);
+    }
+}
