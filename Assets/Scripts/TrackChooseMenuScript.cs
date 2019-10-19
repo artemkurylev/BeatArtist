0d1d95251a1d557c73163986c8984444b079ae1a
Fenrir7Asteron <b.fedotov@innopolis.ru>
2019-10-19 17:39:28 +0300

    Major track selection refactor.

--- Assets/Scripts/TrackChooseMenuScript.cs
+++ Assets/Scripts/TrackChooseMenuScript.cs
@@ -3,25 +3,65 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TrackSelection;
 using Resources;
+using UnityEditor;
 
 public class TrackChooseMenuScript : MonoBehaviour
 {
     public RawImage MenuBackground;
+    public Text ChosenTrackName;
+    public TrackChooseMenuScript Controller;
 
     private SelectedTrack SelectedTrack = SelectedTrack.GetInstance();
     private ResourceLoader ResourceLoader = new ResourceLoader();
+    private Image PreviousHighlighted;
 
 
-    public void OnClickBack()
+    void Start()
     {
-        int MainMenu = 0;
-        SceneManager.LoadScene(MainMenu);
+        if (Controller)
+        {
+            Controller.OnClickTrack(1);
+            GameObject.Find("Scrollbar").GetComponent<Scrollbar>().value = 1.0f;
+        }
     }
 
 
     public void OnClickTrack(int trackId)
     {
         SelectedTrack.SetId(trackId);
+        var track = GameObject.Find("Track" + trackId.ToString());
+
+        var texts = track.GetComponentsInChildren<Text>();
+        foreach (Text text in texts)
+        {
+            if (text.name == "Song name")
+            {
+                ChosenTrackName.text = text.text;
+            }
+        }
+        
+        var images = track.GetComponentsInChildren<Image>();
+        foreach (Image img in images)
+        {
+            Debug.Log(img.name);
+            if (img.name == "Highlight")
+            {
+                string htmlValue = "#7ABAF2";
+                Color newCol;
+                if (ColorUtility.TryParseHtmlString(htmlValue, out newCol))
+                {
+                    newCol.a = 0.4f;
+                    img.color = newCol;
+
+                    if (PreviousHighlighted && PreviousHighlighted != img)
+                    {
+                        PreviousHighlighted.color = Color.clear;
+                    }
+
+                    PreviousHighlighted = img;
+                }
+            }
+        }
         StartCoroutine(ResourceLoader.GetMenuTrackBackground(trackId, MenuBackground));
     }
 
@@ -34,5 +74,11 @@ public class TrackChooseMenuScript : MonoBehaviour
             SelectedTrack.SetId(0);
             LevelChanger.Instance.FadeToLevel(chosenTrack);
         }
+    }
+    
+    
+    public void OnClickBack()
+    {
+        Debug.Log("Back button is pressed!");
     }
 }
