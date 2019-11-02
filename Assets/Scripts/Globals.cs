8682cdb4c8b090fa38e21736f6426868974e542b
Fenrir7Asteron <b.fedotov@innopolis.ru>
2019-11-02 16:27:21 +0300

    Made targets to dissapear in the order of their arrival either on hit or miss. Also added static class for global variables.

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Globals
+{
+    public static int nextClickableTarget = 0;
+    public static float lastUpdateTime = 0;
+    
+    // Минимальное расстояние во времени между двумя нажатиями на экран.
+    // Не позволяет исчезнуть всем целям по одному нажатию.
+    public static readonly float minTimeDifference = 0.01f;
+    public static readonly bool DeveloperMode = (Application.platform != RuntimePlatform.Android); 
+}
